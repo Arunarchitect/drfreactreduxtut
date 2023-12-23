9824d10745fab1f7d0ba628695d806efd4330578
Arunarchitect <intformbase@gmail.com>
2023-12-23 15:44:41 +0530

    added user authentication in postman all

--- backend/tutorial/settings.py
+++ backend/tutorial/settings.py
@@ -182,3 +182,13 @@ DJOSER = {
         'user_delete': "djoser.serializers.UserDeleteSerializer",      
     },
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USE_TLS = True
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = "info@modelflick.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "MOdelflick"
